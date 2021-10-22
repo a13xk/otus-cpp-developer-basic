d5963fe9c209d8fdae33657642d35cc4966fc9a3
Alexander Knyazev <a.knyazev@omp.ru>
2021-10-22 14:44:42 +0300

    Add initial implementation for 'guest the number' game

---
+++ Lesson_06/guess_the_number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+int main()
+{
+    const int target_value = 54;
+    int current_value = 0;
+    bool not_win = true;
+
+    std::cout << "╔═══════════════════════╗" << std::endl;
+    std::cout << "║ Guess The Number Game ║" << std::endl;
+    std::cout << "╚═══════════════════════╝" << std::endl;
+    std::cout << "Enter your guess:" << std::endl;
+
+    do {
+        std::cin >> current_value;
+
+        if (current_value < target_value) {
+            std::cout << "You entered " << current_value << ". It is less than guessed number, please try again" << std::endl;
+        }
+        else if (current_value > target_value) {
+            std::cout << "You entered " << current_value << ". It is greater than guessed number, please try again" << std::endl;
+        }
+        else {
+            std::cout << "Congratulation! You guessed correct number!" << std::endl;
+            break;
+        }
+
+    } while(true);
+
+    return 0;
+}
