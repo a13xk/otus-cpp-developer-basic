2fa203a11ba90e42392ba183679ad90ebff875e3
Alexander Knyazev <a.knyazev@omp.ru>
2021-11-10 10:38:19 +0300

    Add calculation of arithmetic mean value of a sequence

--- Month_02/Lesson_10/src/main.cpp
+++ Month_02/Lesson_10/src/main.cpp
@@ -1,12 +1,14 @@
 #include "CMin.h"
 #include "CMax.h"
+#include "CMean.h"
 
 int main() {
-    const size_t statistics_count = 2;
+    const size_t statistics_count = 3;
     IStatistics *statistics[statistics_count];
 
     statistics[0] = new CMin{};
     statistics[1] = new CMax{};
+    statistics[2] = new CMean{};
 
     double val = 0;
     while (std::cin >> val) {
