13006bb359be876c0e6a9a2eadfc7b197a29e0c1
Alexander Knyazev <a.knyazev@omp.ru>
2021-11-11 12:44:45 +0300

    Reformat and add comments in Lexer class declaration

--- Month_01/Lesson_09/include/lexer.hpp
+++ Month_01/Lesson_09/include/lexer.hpp
@@ -1,27 +1,24 @@
-// OTUS C++ Basic course homework skeleton.
-// Lexer interface
-
 #pragma once
-
 #include <istream>
 #include <string>
 
+/**
+ *  Lexer interface
+ */
 class Lexer {
   public:
     enum class Token {
-        Number,
-        Operator,
-        End,
-        LParenthesis,
-        RParenthesis,
-        Name,
+        Number,         // Integer numbers
+        Operator,       // Arithmetic operators: +-*/
+        End,            // End Of File or newline, i.e. no more data in input stream
+        LParenthesis,   // '(' character
+        RParenthesis,   // ')' character
+        Name,           // Latin alphabet characters
     };
 
-    explicit Lexer(std::istream &in);
-
-    Lexer(const Lexer &other) = delete;
-
-    Lexer &operator=(const Lexer &other) = delete;
+    explicit Lexer(std::istream &in);               // Initialize object of type Lexer explicitly
+    Lexer(const Lexer &other) = delete;             // Prevent initialization with other object of type Lexer
+    Lexer &operator=(const Lexer &other) = delete;  // Prevent '=' operator usage
 
     Token next_token();
 
@@ -31,18 +28,18 @@ class Lexer {
 
   protected:
     bool is_parenthesis(char ch) const;
-
     bool is_operator(char ch) const;
 
   private:
+    /**
+     * Inner state of lexer
+     */
     enum class State {
-        Empty,
-        ReadNumber,
-        ReadName,
-        End,
+        Empty,          // Start reading symbols
+        ReadNumber,     // Read numbers from stdin
+        ReadName,       // Read characters from stdin
+        End,            // Stop reading symbols
     };
-    char next_char();
-    bool is_end() const;
 
     State state_;
     std::string name_;
@@ -50,4 +47,7 @@ class Lexer {
     std::string operator_;
     char ch_;
     std::istream &in_;
+
+    char next_char();
+    bool is_end() const;
 };
