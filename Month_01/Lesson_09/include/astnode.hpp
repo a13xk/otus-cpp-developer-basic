f78ea1377490b8fb77ae832e194c00b58fcc73ce
Alexander Knyazev <a.knyazev@omp.ru>
2021-11-11 16:54:05 +0300

    Reformat ASTNode class declaration

--- Month_01/Lesson_09/include/astnode.hpp
+++ Month_01/Lesson_09/include/astnode.hpp
@@ -9,24 +9,19 @@
 
 class ASTNode {
   public:
-    explicit ASTNode(const std::string &repr);
-
+    explicit ASTNode(const std::string &repr);          // Initialize object of type ASTNode explicitly
     ASTNode(const std::string &repr, ASTNode *lhs, ASTNode *rhs);
-
-    ASTNode(const ASTNode &other) = delete;
-
-    ASTNode &operator=(const ASTNode &other) = delete;
-
+    ASTNode(const ASTNode &other) = delete;             // Prevent initialization with other object of type ASTNode
+    ASTNode &operator=(const ASTNode &other) = delete;  // Prevent '=' operator usage
     ~ASTNode();
 
     std::string repr() const;
-
     void print(std::ostream &out) const;
 
   private:
-    void inner_print(std::ostream &out, size_t indent) const;
-
     std::string repr_;
     ASTNode *lhs_;
     ASTNode *rhs_;
+
+    void inner_print(std::ostream &out, size_t indent) const;
 };
