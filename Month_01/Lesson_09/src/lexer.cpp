1cd28c8c45c04fcd144fb480e9277d8d97d7e840
Alexander Knyazev <a.knyazev@omp.ru>
2021-11-11 12:44:00 +0300

    Reformat and add comments of Lexer::next_token method

--- Month_01/Lesson_09/src/lexer.cpp
+++ Month_01/Lesson_09/src/lexer.cpp
@@ -55,71 +55,77 @@ bool Lexer::is_operator(char ch) const {
 bool Lexer::is_parenthesis(char ch) const {
     return ch == '(' || ch == ')';
 }
-
+/**
+ * Parse the next token from input stream and return its type
+ */
 Lexer::Token Lexer::next_token() {
     for (;;) {
         switch (state_) {
-        case State::End:
-            return Token::End;
-        case State::ReadNumber:
-            if (is_end()) {
-                state_ = State::End;
-                return Token::Number;
-            }
-            if (std::isdigit(ch_)) {
-                number_ = 10 * number_ + (ch_ - '0');
-                state_ = State::ReadNumber;
-                next_char();
-                break;
-            }
-            state_ = State::Empty;
-            return Token::Number;
-        case State::ReadName:
-            if (is_end()) {
-                state_ = State::End;
-                return Token::Name;
-            }
-            if (std::isalpha(ch_) || std::isdigit(ch_)) {
-                name_ += ch_;
-                next_char();
-                break;
-            }
-            state_ = State::Empty;
-            return Token::Name;
-        case State::Empty:
-            if (is_end()) {
-                state_ = State::End;
+            case State::End:
                 return Token::End;
-            }
-            if (std::isspace(ch_)) {
-                next_char();
-                break;
-            }
-            if (is_operator(ch_)) {
-                operator_ = ch_;
-                next_char();
-                return Token::Operator;
-            }
-            if (ch_ == '(') {
-                next_char();
-                return Token::LParenthesis;
-            }
-            if (ch_ == ')') {
-                next_char();
-                return Token::RParenthesis;
-            }
-            if (std::isdigit(ch_)) {
-                number_ = ch_ - '0';
-                state_ = State::ReadNumber;
-                next_char();
-                break;
-            }
-            if (std::isalpha(ch_)) {
-                name_ = ch_;
-                state_ = State::ReadName;
-                next_char();
-                break;
-            }
+            case State::ReadNumber:
+                if (is_end()) {
+                    state_ = State::End;
+                    return Token::Number;
+                }
+                if (std::isdigit(ch_)) {
+                    // ASCII '0' = 48, this will convert ASCII code of digit character to its integer value
+                    // If number consists of 2 or more digits (tens, hundreds, ...), each digit to the left
+                    // has to be multiplied by 10 to represent actual value of number
+                    number_ = 10 * number_ + (ch_ - '0');
+                    state_ = State::ReadNumber;
+                    next_char();
+                    break;
+                }
+                state_ = State::Empty;
+                return Token::Number;
+            case State::ReadName:
+                if (is_end()) {
+                    state_ = State::End;
+                    return Token::Name;
+                }
+                if (std::isalpha(ch_) || std::isdigit(ch_)) {
+                    name_ += ch_;
+                    next_char();
+                    break;
+                }
+                state_ = State::Empty;
+                return Token::Name;
+            case State::Empty:
+                if (is_end()) {
+                    state_ = State::End;
+                    return Token::End;
+                }
+                if (std::isspace(ch_)) {
+                    next_char();
+                    break;
+                }
+                if (is_operator(ch_)) {
+                    operator_ = ch_;
+                    next_char();
+                    return Token::Operator;
+                }
+                if (ch_ == '(') {
+                    next_char();
+                    return Token::LParenthesis;
+                }
+                if (ch_ == ')') {
+                    next_char();
+                    return Token::RParenthesis;
+                }
+                if (std::isdigit(ch_)) {
+                    // ASCII '0' = 48, this will convert ASCII code of digit character to its integer value
+                    number_ = ch_ - '0';
+                    state_ = State::ReadNumber;
+                    next_char();
+                    break;
+                }
+                if (std::isalpha(ch_)) {
+                    name_ = ch_;
+                    state_ = State::ReadName;
+                    next_char();
+                    break;
+                }
         }
     }
 }
