c295736af24a99e8ae2ad8bda6eae0f2518bd355
Alexander Knyazev <a.knyazev@omp.ru>
2021-11-12 23:35:37 +0300

    Move Number constructor and value definition to .cpp file

--- Month_01/Lesson_09/include/number.hpp
+++ Month_01/Lesson_09/include/number.hpp
@@ -6,11 +6,8 @@
 
 class Number : public ASTNode {
   public:
-    Number(int val)
-        : ASTNode(std::to_string(val))
-        , val_(val) {}
-
-    int value() const { return val_; }
+    explicit Number(int val);
+    int value() const;
 
   private:
     int val_;
