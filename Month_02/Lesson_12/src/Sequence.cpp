7ff1d5b1143a35725d9be1e20415138841b0da31
Alexander Knyazev <{ID}+{username}@users.noreply.github.com>
2021-12-06 22:33:06 +0300

    Add Sequence class draft implementation

---
+++ Month_02/Lesson_12/src/Sequence.cpp
@@ -0,0 +1,53 @@
+#include "IContainer.h"
+
+// Template class declaration
+template <typename T>
+class Sequence : public IContainer<T>{
+public:
+    Sequence();
+    ~Sequence() override;
+    void push_back(T value) override;
+
+private:
+    int m_size;
+    T* m_region;
+};
+
+// Template class definition
+/**
+ * Default constructor
+ * @tparam T Type of container elements
+ */
+template<typename T>
+Sequence<T>::Sequence() : m_size(0), m_region(nullptr) {};
+
+/**
+ * Trivial destructor
+ * @tparam T Type of container elements
+ */
+template<typename T>
+Sequence<T>::~Sequence() = default;
+
+/**
+ * Add new element to container
+ * @tparam T New element type
+ * @param value New element value
+ */
+template<typename T>
+void Sequence<T>::push_back(T value) {
+    T* new_region = new T[m_size + 1];  // allocate new memory
+    for (int i = 0; i < m_size; ++i) {
+        new_region[i] = m_region[i];    // copy existing elements
+    }
+    new_region[m_size] = value;         // add new element
+    delete [] m_region;                 // delete old allocated memory
+    m_region = new_region;              // save new allocated memory in class member
+    m_size += 1;                        // update container size
+}
+
+
+
+
+
+
+
