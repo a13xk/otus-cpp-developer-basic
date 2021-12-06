cf39b1da9a6f64c07bcc76dd50e4d073e0eaa8c9
Alexander Knyazev <{ID}+{username}@users.noreply.github.com>
2021-12-06 20:26:59 +0300

    Add todos to main.cpp

--- Month_02/Lesson_12/src/main.cpp
+++ Month_02/Lesson_12/src/main.cpp
@@ -1,3 +1,31 @@
 int main() {
+    // TODO: Consecutive container
+    // TODO: 1. create container to store objects of type int
+    // TODO: 2. Add 10 elements to container (0,1..9)
+    // TODO: 3. Print container elements. Exp:  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    // TODO: 4. Print container size. Exp: 10
+    // TODO: 5. Delete 3rd, 5th and 7th element from container
+    // TODO: 6. Print container elements. Exp:  0, 1, 3, 5, 7, 8, 9
+    // TODO: 7. Add 10 to the beginning of container
+    // TODO: 8. Print container elements. Exp:  10, 0, 1, 3, 5, 7, 8, 9
+    // TODO: 9. Add 20 to the middle of container
+    // TODO: 10. Print container elements. Exp:  10, 0, 1, 3, 20, 5, 7, 8, 9
+    // TODO: 11. Add 30 to the end of container
+    // TODO: 12. Print container elements. Exp:  10, 0, 1, 3, 20, 5, 7, 8, 9, 30
+
+    // TODO: List container
+    // TODO: 1. create container to store objects of type int
+    // TODO: 2. Add 10 elements to container (0,1..9)
+    // TODO: 3. Print container elements. Exp:  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    // TODO: 4. Print container size. Exp: 10
+    // TODO: 5. Delete 3rd, 5th and 7th element from container
+    // TODO: 6. Print container elements. Exp:  0, 1, 3, 5, 7, 8, 9
+    // TODO: 7. Add 10 to the beginning of container
+    // TODO: 8. Print container elements. Exp:  10, 0, 1, 3, 5, 7, 8, 9
+    // TODO: 9. Add 20 to the middle of container
+    // TODO: 10. Print container elements. Exp:  10, 0, 1, 3, 20, 5, 7, 8, 9
+    // TODO: 11. Add 30 to the end of container
+    // TODO: 12. Print container elements. Exp:  10, 0, 1, 3, 20, 5, 7, 8, 9, 30
+
     return 0;
 }
